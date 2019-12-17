e54c1bcc2b6f45ef2dd85db749225e9a4558a996
Paul <paul.barbaux@gmail.com>
2019-12-17 16:58:15 +0100

    adding login form and animate it

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -57,3 +57,78 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.body.style.backgroundColor = "white";
 }
+
+
+$(window, document, undefined).ready(function() {
+  $('form#login').submit(function(){
+    $(this).find(':input[type=submit]').prop('disabled', true);
+});
+  $(".login-modal").hide();
+
+  // Show overlay & Open modal
+
+  $( ".close_login" ).click(function() {
+    $(".login-modal").fadeOut(200);
+    $(".logo").animate({'top': "70px","z-index":"1"});
+  });
+  $( ".connect_show" ).click(function() {
+    closeNav();
+    $(".login-modal").fadeIn(200);
+    $(".logo").animate({'top': "-160px","z-index":"99999"});
+  });
+  $(".login-modal").click(function(event) {
+    event.stopPropagation();
+  });
+
+
+  $('#login').submit(function() { // catch the form's submit event
+    $.ajax({ // create an AJAX call...
+        data: $(this).serialize(), // get the form data
+        type: $(this).attr('method'), // GET or POST
+        url: $(this).attr('action'), // the file to call
+        success: function(response) { // on success..
+            $("#debug").html(response); // update the DIV
+        }
+    });
+    return false; // cancel original event to prevent form submitting
+});
+
+
+  // Input label
+
+  $('input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+
+
+  // Animations
+
+  var $ripples = $('.ripples');
+
+  $ripples.on('click.Ripples', function(e) {
+
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.ripplesCircle');
+
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
+
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    $this.addClass('is-active');
+
+  });
+
+  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+  	$(this).removeClass('is-active');
+  });
+
+});
--- www/php/connexion.php
+++ www/php/connexion.php
@@ -1,6 +1,17 @@
 <?php
-
-
+if(isset($_post['pass']))
+{
+  $pass = $_post['pass'];
+  echo "<p>{$pass}</p>";
+  $pass_hash = md5($pass);
+  $pass_hash = sha1($pass_hash);
+  echo "<p>{$pass_hash}</p>";
+}
+echo "<p>no data</p>";
 
 
  ?>
+ <form class="" action="" method="post">
+   <input type="text" name="pass" value="">
+   <input type="submit" name="" value="">
+ </form>
